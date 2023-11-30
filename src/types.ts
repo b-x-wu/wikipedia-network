7d3be6391fae98850bb216ccd4f63d092a0f5053
bruce-x-wu <wu.bruce.x@gmail.com>
2023-11-29 20:51:39 -0800

    config: clean up environment for changes

--- src/types.ts
+++ src/types.ts
@@ -1,28 +1,4 @@
-export type PageSlug = string
-
 export interface Page {
-    readonly slug: PageSlug // encoded fragment of url identifying the page
-    readonly isRedirect: boolean // true if fetching the page gives 302. false otherwise
-}
-
-export interface PageEdge {
-    readonly from: PageSlug
-    readonly to: PageSlug
-}
-
-export interface PageNode {
-    readonly page: Page
-    outSlugs: PageSlug[]
-}
-
-export const PAGE_PREFIX = 'https://en.wikipedia.org/wiki/'
-
-export const PAGE_SLUGS_NAME = "page_urls.txt"
-
-export const getUrlFromPageSlug = (pageSlug: PageSlug): string => {
-    return PAGE_PREFIX + pageSlug
-}
-
-export const getUrlFromPage = (page: Page): string => {
-    return getUrlFromPageSlug(page.slug)
+    readonly title: string
+    readonly isRedirect: boolean
 }
