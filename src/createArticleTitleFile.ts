d89056e27e5296404d8aa7994054d2b6d4776306
bruce-x-wu <wu.bruce.x@gmail.com>
2023-12-05 23:29:52 -0800

    feat: write script for generating article titles file

--- src/createArticleTitleFile.ts
+++ src/createArticleTitleFile.ts
@@ -2,27 +2,24 @@ import bz2 from "unbzip2-stream"
 import { ReadStream, createReadStream, createWriteStream } from 'fs'
 import * as dotenv from "dotenv"
 import { WikipediaStream } from "./wikipediaStream"
-import { getPageNodeFromPage, getQueryStringsFromPageNode } from "./pageUtils"
 dotenv.config()
 
 const WIKIPEDIA_ZIP_FILE_NAME = 'enwiki-20230820-pages-articles-multistream.xml.bz2'
-const TRANSACTIONS_FILE_NAME = 'transactions.csv'
-const TRANSACTIONS_FILE_DELIMITER = String.fromCharCode(31)
+const ARTICLE_TITLE_FILE_NAME = 'article-titles.txt'
 const BUFFER_CAPACITY = 10000
 
 const main = async () => {
     let stream: ReadStream | undefined
-    let transactionsWriteStream = createWriteStream(TRANSACTIONS_FILE_NAME)
-    transactionsWriteStream.cork()
-
+    let articleTitleWriteStream = createWriteStream(ARTICLE_TITLE_FILE_NAME)
+    articleTitleWriteStream.cork()
     let bufferSize: number = 0
 
     const onProgramEnd = async (err?: Error) => {
         console.log('ending program')
         if (err != null) console.error(err)
-        console.log(`WRITING OUT ${bufferSize} TRANSACTIONS`)
-        transactionsWriteStream.close()
-        if (stream != null) stream.destroy()
+        console.log(`FLUSHING BUFFER [bufferSize=${bufferSize}]`)
+        articleTitleWriteStream.close()
+        if (stream != null) stream.close()
     }
 
     process.on('SIGINT', onProgramEnd)
@@ -30,18 +27,18 @@ const main = async () => {
 
     const wikipediaStream: WikipediaStream = new WikipediaStream()
     wikipediaStream.on("page", (page) => {
-        const pageNode = getPageNodeFromPage(page)
-        const queries = getQueryStringsFromPageNode(pageNode)
-        const queriesString = queries.join(TRANSACTIONS_FILE_DELIMITER) + '\n'
+        if (page.namespace !== 0) return
 
-        transactionsWriteStream.write(queriesString)
+        if (Math.random() < 0.001) console.log(`Writing out ${page.title}`)
+
+        articleTitleWriteStream.write(page.title + '\n')
         bufferSize += 1
 
         if (bufferSize === BUFFER_CAPACITY) {
-            console.log(`WRITING OUT ${bufferSize} TRANSACTIONS`)
-            process.nextTick(() => transactionsWriteStream.uncork())
+            console.log(`FLUSHING BUFFER [bufferSize=${bufferSize}]`)
+            process.nextTick(() => articleTitleWriteStream.uncork())
             bufferSize = 0
-            process.nextTick(() => transactionsWriteStream.cork())
+            process.nextTick(() => articleTitleWriteStream.cork())
         }
     })
 
